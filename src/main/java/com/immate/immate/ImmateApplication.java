6e5e0a60448c2ccb9181bfd96874675807d12ffd
GyuHo123 <kyuhokim12@gmail.com>
2025-01-02 22:00:57 +0900

    init

---
+++ src/main/java/com/immate/immate/ImmateApplication.java
@@ -0,0 +1,13 @@
+package com.immate.immate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmateApplication.class, args);
+	}
+
+}
