5dda22043efd5923f5fc00126d1e2db2b1134dba
GyuHo123 <kyuhokim12@gmail.com>
2025-01-04 01:07:08 +0900

    feat : Add JWT-based security configuration
    This commit introduces Spring Security with JWT authentication. It includes `CustomUserDetailsService`, `JwtAuthenticationFilter`, and `JwtTokenProvider` for handling user authentication. The `SecurityConfig` class is updated alongside necessary dependencies in `build.gradle.kts`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,10 +28,16 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-redis")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
